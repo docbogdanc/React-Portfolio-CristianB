41672896b1d7777f9c38edef8098940c681653cc
unknown <docbogdanc@yahoo.com>
2024-03-05 12:20:50 +0000

    bootstrap for navlink added

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,62 +3,123 @@ import { Link, NavLink } from 'react-router-dom';
 import './style.css';
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
-function Navbar() {
+// function Navbar() {
+//   return (
+//     <nav className="navbar navbar-expand-lg navbar-light bg-light">
+//       <div className="container-fluid">
+//         <Link className="navbar-brand" to="/">
+//           Cristian Bogdan
+//         </Link>
+//         <button
+//           className="navbar-toggler"
+//           type="button"
+//           data-bs-toggle="collapse"
+//           data-bs-target="#navbarNav"
+//           aria-controls="navbarNav"
+//           aria-expanded="false"
+//           aria-label="Toggle navigation"
+//         >
+//           <span className="navbar-toggler-icon"></span>
+//         </button>
+//         <div className="collapse navbar-collapse" id="navbarNav">
+//           <ul className="navbar-nav">
+//             <li className="nav-item">
+//               <NavLink
+//                 to="/"
+//                 end
+//                 className={({ isActive }) =>
+//                   isActive ? 'nav-link active' : 'nav-link'
+//                 }
+//               >
+//                 About
+//               </NavLink>
+//             </li>
+//             <li className="nav-item">
+//               <NavLink
+//                 to="/work"
+//                 className={({ isActive }) =>
+//                   isActive ? 'nav-link active' : 'nav-link'
+//                 }
+//               >
+//                 Work
+//               </NavLink>
+//             </li>
+//             <li className="nav-item">
+//               <NavLink
+//                 to="/contact"
+//                 className={({ isActive }) =>
+//                   isActive ? 'nav-link active' : 'nav-link'
+//                 }
+//               >
+//                 Contact
+//               </NavLink>
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// }
+
+// export default Navbar;
+
+function NavTabs() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
-          Cristian Bogdan
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <NavLink
+          to="/"
+          end
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
         >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <NavLink
-                to="/"
-                end
-                className={({ isActive }) =>
-                  isActive ? 'nav-link active' : 'nav-link'
-                }
-              >
-                About
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                to="/work"
-                className={({ isActive }) =>
-                  isActive ? 'nav-link active' : 'nav-link'
-                }
-              >
-                Work
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                to="/contact"
-                className={({ isActive }) =>
-                  isActive ? 'nav-link active' : 'nav-link'
-                }
-              >
-                Contact
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+          Home
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="about"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          About
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="work"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          work
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="contact"
+          end
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Contact
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="contact/learn"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Learn
+        </NavLink>
+      </li>
+    </ul>
   );
 }
 
-export default Navbar;
+export default NavTabs;
