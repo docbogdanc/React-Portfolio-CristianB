f9cf6c4ceee31fad5342ef802e77b453283bcece
docbogdanc <docbogdanc@yahoo.com>
2024-04-24 18:04:06 +0100

    gallery rearranged and description added

--- src/pages/EachProject.jsx
+++ src/pages/EachProject.jsx
@@ -9,15 +9,19 @@ function OneProject (props) {
     const selectedProject = projectData[props.index]
   return (
     <div className='row' style={{ border: '1px solid black', marginTop: '10px', padding: '10px', backgroundColor: '#f4f3ef' }}>
-      <div className='col-6 col-sm-4'>
-        <img src={selectedProject.img} alt="Project Screenshot" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+      <div className='col-12 col-sm-12 col-lg-4'>
+        <img src={selectedProject.img} alt="Project Screenshot" style={{ width: '100%', height: '100%', objectFit: 'contain' }} />
       </div>
-      
-      <div className='col-6 col-sm-4'>
-        <h2>{selectedProject.title}</h2>
-        <p>Link to deployed version: <a href={selectedProject.deployedLink}>{selectedProject.title}</a></p>
-        <p>Link to GitHub repository: <a href={selectedProject.githubLink}>{selectedProject.githubLink}</a></p>
+  
+      <div className='col-12 col-sm-12 col-lg-4'>
+        <p>Deployed version: <a href={selectedProject.deployedLink}>{selectedProject.title}</a></p>
+        <p>GitHub repository: <a href={selectedProject.githubLink}>{selectedProject.githubLink}</a></p>
         {/* Add more project details as needed */}
+      </div>
+
+      <div className='col-12 col-sm-12 col-lg-4'>
+        <p>{selectedProject.description1}</p>
+        <p>{selectedProject.description2}</p>
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,15 @@ import Hero from '../components/Hero';
 import Container from '../components/Container';
 import Row from '../components/Row';
 import Col from '../components/Col';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function About() {
   return (
     <div>
       <Hero>
-        <img src="https://github.com/docbogdanc/Bootstrap-Portfolio-CristianB/blob/main/images/poza.jpg?raw=true" alt="Profile Image" style={{ position: 'absolute', top: 0, left: 0,  height: '250px', margin : '20px' }} />
+        <img src="https://github.com/docbogdanc/Bootstrap-Portfolio-CristianB/blob/main/images/poza.jpg?raw=true" alt="Profile Image" className="img-fluid" style={{ position: 'absolute', top: 0, left: 0,  height: '250px', margin : '20px' }} />
         <h1>Cristian Bogdan</h1>
-        <h5>"The secret of making progress is to get started"</h5>
+        <h5>&quot;The secret of making progress is to get started&quot;</h5>
         <h6> - Mark Twain -</h6>
 
       </Hero>
